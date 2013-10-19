29d76b9cd678875b19eb1440b4490de5dbaf265e
Aaron Raddon <araddon@gmail.com>
2013-10-19 13:16:27 -0700

    update graphite recipe

--- graphite/graphite_install.sh
+++ graphite/graphite_install.sh
@@ -4,6 +4,8 @@
 
 #cp local_settings.py.example local_settings.py
 #python manage.py syncdb  # Follow the prompts, creating a superuser is optional
+cd /opt/graphite/webapp/graphite 
+
 python manage.py syncdb --noinput 
 
 #cd /opt/graphite 
